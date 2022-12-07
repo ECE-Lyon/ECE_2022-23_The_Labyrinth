4bf70415ff5ebf5e6cdde28fc72f291ca8bd871b
jadep <jade.planterose@edu.ece.fr>
2022-12-07 16:08:20 +0100

    test 2 pour fct afficher tableau

--- affTabl2.c
+++ affTabl2.c
@@ -9,25 +9,31 @@
 #define nbrInfosPlateau 4    //int typeCase, orientationCase; char tresor, pion;
 #define nbrInfosJoueur 4    //couleur, trésor recherché (tableau : nbr et le type), trésors obtenus, emplacement
 #define nbrCarteTresor 24
-#define nbrTuileABouger 34
-#define CHARACTERE 219
+#define nbrTuileABouger 34    //sur 49 tuiles
+#define CHARACTERE 219      // █
 
 int joueur = 0;
 int infoJoueur[nbrInfosJoueur] = {0};
 int carteTresor[nbrCarteTresor] = {0};
-int tableau[LARGEURPLATEAU][LONGUEURPLATEAU][nbrInfosPlateau] = {{0}};
+
+typedef struct {int typeCase, orientationCase; char tresor, pion;} Case;
 
 typedef struct {int typeCase, orientationCase, tresor, couleur, absisse, ordonnee;} placementPion;
 
 void AfficherPlateau () {
-    for(int i = 0; i < LARGEURPLATEAU; i++) {
-        for(int l = 0; l < LARGEURCASE; l++) {
-            for (int j = 0; j < LONGUEURPLATEAU; j++) {
-                for (int k = 0; k < LONGUEURCASE; k++) {
+    for(int lap = 0; lap < LARGEURPLATEAU; lap++) {
+        int m = 0;
+        for(int lac = 0; lac < LARGEURCASE; lac++) {
+            for (int lop = 0; lop < LONGUEURPLATEAU; lop++) {
+                if(m == 0) {
+                    //début d'une case
+                }
+                for (int loc = 0; loc < LONGUEURCASE; loc++) {
                     printf("%c ", CHARACTERE);
                 }
                 printf("  ");
             }
+            m += 1;
             printf("\n");
         }
         printf("\n");
@@ -36,6 +42,7 @@ void AfficherPlateau () {
 
 int main(void) {
     srand(time(NULL));
+    Case tableau[LONGUEURPLATEAU][LARGEURPLATEAU] = {{0}};
     AfficherPlateau();
     return 0;
 }

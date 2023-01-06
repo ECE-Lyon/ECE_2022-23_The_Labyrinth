c33263d0d8722e3021c92a64df82a923afcfa3ab
jadep <jade.planterose@edu.ece.fr>
2023-01-06 19:35:44 +0100

    .

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include "startGame.h"
 
+char letter;
+
 int main(void) {
     printf("Welcome + explain games's rules\n\n");
 
     while(1) {
         printf("Main menu:\n");
         printf("1) Start game\n");
-        printf("2) Quit\n");
+        printf("2) Rules\n");
+        printf("3) Quit\n");
         printf("Enter the number of your selection: ");
 
         // Prompt the user to enter their selection
@@ -19,6 +22,11 @@ int main(void) {
                 startGame();
                 break;
             case 2 :
+                while(letter != *"m") {
+                    scanf("%c", &letter);
+                }
+                break;
+            case 3 :
                 return 0;
             default:
                 printf("ERROR");
